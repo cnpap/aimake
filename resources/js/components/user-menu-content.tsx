d7bb5f3479984bc923fa8a10491f55f75b70b737
yang-wei-jie <justLikeSuoLong@outlook.com>
2025-12-29 14:55:30 +0800

    chore: .

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -42,7 +42,7 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
                         onClick={cleanup}
                     >
                         <Settings className="mr-2" />
-                        Settings
+                        设置
                     </Link>
                 </DropdownMenuItem>
             </DropdownMenuGroup>
@@ -56,7 +56,7 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
                     data-test="logout-button"
                 >
                     <LogOut className="mr-2" />
-                    Log out
+                    退出登录
                 </Link>
             </DropdownMenuItem>
         </>
